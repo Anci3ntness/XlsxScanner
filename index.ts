bf8a218d19f5f6b72ecdc29525f72564f61a0156
Anci3ntness <nikitamashkov13@gmail.com>
2024-02-15 22:57:20 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,99 @@
+import fs from "fs"
+import path from "path"
+
+import XLSX from "xlsx"
+
+const dict = [
+	"Январь",
+	"Февраль",
+	"Март",
+	"Апрель",
+	"Май",
+	"Июнь",
+	"Июль",
+	"Август",
+	"Сентябрь",
+	"Октябрь",
+	"Ноябрь",
+	"Декабрь",
+]
+interface IJsonOutput {
+	[name: string]: string
+}
+
+;(function CountXlsx(): void {
+	try {
+		const sumFinalArray: IJsonOutput[] = []
+		fs.readdirSync("./tables", {
+			encoding: "utf8",
+		}).forEach((file) => {
+			if (path.extname(file).toLowerCase() !== ".xlsx") return
+			const JsonTable = parseExcel("./tables/" + file)
+			let concatArray: number[] = []
+			let november = 0
+			for (let month = 1; month <= 12; month++) {
+				const filtered = JsonTable.filter(
+					(date) => Number(date["дата"].split(".")[1]) == month
+				)
+
+				let countArr = [0, 0, 0, 0, 0]
+				filtered.map((value) => {
+					countArr[0] += value["всего кол-во"]
+					countArr[1] += value["БК"]
+					countArr[2] += value["ТК"]
+					countArr[3] += value["СОЦ.К."]
+					countArr[4] += value["ТРОЙКА"]
+
+					if (month === 11) {
+						const day = Number(value["дата"].split(".")[0])
+						const week = value["день недели"]
+
+						if (
+							(day > 6 || day < 4) &&
+							week !== "вс" &&
+							week !== "сб"
+						) {
+							november += value["всего кол-во"]
+						}
+					}
+				})
+				concatArray = concatArray.concat(countArr)
+			}
+			const JsonOutput: IJsonOutput = {}
+			JsonOutput["поток"] = String(november)
+			for (let i = 0; i < concatArray.length; i++) {
+				JsonOutput[dict[~~(i / 5)] + ((i % 5) + 1)] = String(
+					concatArray[i]
+				)
+			}
+
+			sumFinalArray.push(JsonOutput)
+		})
+		writeExcel("output.xlsx", sumFinalArray)
+	} catch (e: any) {
+		throw new Error(e)
+	}
+})()
+
+function parseExcel(filePath: string): any[] {
+	const workBook = XLSX.readFile(filePath)
+
+	let name = workBook.SheetNames[0]
+
+	return XLSX.utils.sheet_to_json(workBook.Sheets[name])
+}
+function writeExcel(
+	filePath: string,
+	list: any[],
+	sheetName = "Выходные данные"
+) {
+	const workBook = XLSX.utils.book_new()
+
+	XLSX.utils.book_append_sheet(
+		workBook,
+		XLSX.utils.json_to_sheet(list),
+		sheetName
+	)
+
+	XLSX.writeFile(workBook, filePath)
+}
