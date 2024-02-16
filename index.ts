17fc21a75ac476394d33693807a781ee0a031335
Anci3ntness <nikitamashkov13@gmail.com>
2024-02-16 20:12:17 +0300

    Add birthtime sort

--- index.ts
+++ index.ts
@@ -28,53 +28,61 @@ interface IJsonOutput {
 		const sumFinalArray: IJsonOutput[] = []
 		fs.readdirSync("./tables", {
 			encoding: "utf8",
-		}).forEach((file) => {
-			if (path.extname(file).toLowerCase() !== ".xlsx") return
-			const JsonTable = parseExcel("./tables/" + file)
-			let concatArray: number[] = []
-			let november = 0
-			for (let month = 1; month <= 12; month++) {
-				const filtered = JsonTable.filter(
-					(date) => Number(date["дата"].split(".")[1]) == month
-				)
-
-				let countArr = [0, 0, 0, 0, 0]
-				filtered.map((value) => {
-					countArr[0] += value["всего кол-во"]
-					countArr[1] += value["БК"]
-					countArr[2] += value["ТК"]
-					countArr[3] += value["СОЦ.К."]
-					countArr[4] += value["ТРОЙКА"]
-
-					if (month === 11) {
-						const day = Number(value["дата"].split(".")[0])
-						const week = value["день недели"]
-
-						if (
-							(day > 6 || day < 4) &&
-							week !== "вс" &&
-							week !== "сб"
-						) {
-							november += value["всего кол-во"]
-						}
-					}
-				})
-				concatArray = concatArray.concat(countArr)
-			}
-			const JsonOutput: IJsonOutput = {}
-			JsonOutput["Файл"] = path.parse(file).name
-			JsonOutput["Поток"] = String(november)
-			for (let i = 0; i < concatArray.length; i++) {
-				JsonOutput[dict[~~(i / 5)] + ((i % 5) + 1)] = String(
-					concatArray[i]
-				)
-			}
-
-			sumFinalArray.push(JsonOutput)
-			console.info(
-				"Файл '" + path.parse(file).name + "' обработан успешно!"
-			)
 		})
+			.filter((file) => {
+				return new Promise((resolve, reject) => {
+					fs.stat("./tables/" + file, (err, stats) => {
+						resolve(new Date(stats.birthtime).getTime())
+					})
+				})
+			})
+			.forEach((file) => {
+				if (path.extname(file).toLowerCase() !== ".xlsx") return
+				const JsonTable = parseExcel("./tables/" + file)
+				let concatArray: number[] = []
+				let november = 0
+				for (let month = 1; month <= 12; month++) {
+					const filtered = JsonTable.filter(
+						(date) => Number(date["дата"].split(".")[1]) == month
+					)
+
+					let countArr = [0, 0, 0, 0, 0]
+					filtered.map((value) => {
+						countArr[0] += value["всего кол-во"]
+						countArr[1] += value["БК"]
+						countArr[2] += value["ТК"]
+						countArr[3] += value["СОЦ.К."]
+						countArr[4] += value["ТРОЙКА"]
+
+						if (month === 11) {
+							const day = Number(value["дата"].split(".")[0])
+							const week = value["день недели"]
+
+							if (
+								(day > 6 || day < 4) &&
+								week !== "вс" &&
+								week !== "сб"
+							) {
+								november += value["всего кол-во"]
+							}
+						}
+					})
+					concatArray = concatArray.concat(countArr)
+				}
+				const JsonOutput: IJsonOutput = {}
+				JsonOutput["Файл"] = path.parse(file).name
+				JsonOutput["Поток"] = String(november)
+				for (let i = 0; i < concatArray.length; i++) {
+					JsonOutput[dict[~~(i / 5)] + ((i % 5) + 1)] = String(
+						concatArray[i]
+					)
+				}
+
+				sumFinalArray.push(JsonOutput)
+				console.info(
+					"Файл '" + path.parse(file).name + "' обработан успешно!"
+				)
+			})
 		writeExcel("./output.xlsx", sumFinalArray)
 	} catch (e: any) {
 		console.warn("Произошла ошибка при обработке файла")
